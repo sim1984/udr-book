c3c2ce811789b20785ac37245a2410d67029bc8a
Simonov Denis <sim-mail@list.ru>
2019-03-02 15:54:53 +0300

    UdrFactories

--- examples/Common/UdrFactories.pas
+++ examples/Common/UdrFactories.pas
@@ -1,7 +1,7 @@
 unit UdrFactories;
 
 {$IFDEF FPC}
-  {$MODE DELPHI}{$H+}
+{$MODE DELPHI}{$H+}
 {$ENDIF}
 
 interface
@@ -10,9 +10,9 @@ uses SysUtils, Firebird;
 
 type
 
-
-  TFunctionSimpleFactory<T: IExternalFunctionImpl, constructor> =
-  class(IUdrFunctionFactoryImpl)
+  // Простая фабрика внешних функций
+  TFunctionSimpleFactory<T: IExternalFunctionImpl, constructor> = class
+    (IUdrFunctionFactoryImpl)
     procedure dispose(); override;
 
     procedure setup(AStatus: IStatus; AContext: IExternalContext;
@@ -23,11 +23,14 @@ type
       AMetadata: IRoutineMetadata): IExternalFunction; override;
   end;
 
+  // Внешняя функция с метаданными
   TExternalFunction = class(IExternalFunctionImpl)
     Metadata: IRoutineMetadata;
   end;
 
-  TFunctionFactory<T: TExternalFunction, constructor> = class(IUdrFunctionFactoryImpl)
+  // Фабрика внешних функций с метаданными
+  TFunctionFactory<T: TExternalFunction, constructor> = class
+    (IUdrFunctionFactoryImpl)
     procedure dispose(); override;
 
     procedure setup(AStatus: IStatus; AContext: IExternalContext;
@@ -38,8 +41,9 @@ type
       AMetadata: IRoutineMetadata): IExternalFunction; override;
   end;
 
-  TProcedureSimpleFactory<T: IExternalProcedureImpl, constructor> =
-  class(IUdrProcedureFactoryImpl)
+  // Простая фабрика внешних процедур
+  TProcedureSimpleFactory<T: IExternalProcedureImpl, constructor> = class
+    (IUdrProcedureFactoryImpl)
     procedure dispose(); override;
 
     procedure setup(AStatus: IStatus; AContext: IExternalContext;
@@ -50,11 +54,14 @@ type
       AMetadata: IRoutineMetadata): IExternalProcedure; override;
   end;
 
+  // Внешняя процедура с метаданными
   TExternalProcedure = class(IExternalProcedureImpl)
     Metadata: IRoutineMetadata;
   end;
 
-  TProcedureFactory<T: TExternalProcedure, constructor> = class(IUdrProcedureFactoryImpl)
+  // Фабрика внешних процедур с метаданными
+  TProcedureFactory<T: TExternalProcedure, constructor> = class
+    (IUdrProcedureFactoryImpl)
     procedure dispose(); override;
 
     procedure setup(AStatus: IStatus; AContext: IExternalContext;
@@ -63,6 +70,35 @@ type
 
     function newItem(AStatus: IStatus; AContext: IExternalContext;
       AMetadata: IRoutineMetadata): IExternalProcedure; override;
+  end;
+
+  // Простая фабрика внешних триггеров
+  TTriggerSimpleFactory<T: IExternalTriggerImpl, constructor> = class
+    (IUdrTriggerFactoryImpl)
+    procedure dispose(); override;
+
+    procedure setup(AStatus: IStatus; AContext: IExternalContext;
+      AMetadata: IRoutineMetadata; AFieldsBuilder: IMetadataBuilder); override;
+
+    function newItem(AStatus: IStatus; AContext: IExternalContext;
+      AMetadata: IRoutineMetadata): IExternalTrigger; override;
+  end;
+
+  // Внешний триггер с метаданными
+  TExternalTrigger = class(IExternalTriggerImpl)
+    Metadata: IRoutineMetadata;
+  end;
+
+  // Фабрика внешних триггеров с метаданными
+  TTriggerFactory<T: TExternalTrigger, constructor> = class
+    (IUdrTriggerFactoryImpl)
+    procedure dispose(); override;
+
+    procedure setup(AStatus: IStatus; AContext: IExternalContext;
+      AMetadata: IRoutineMetadata; AFieldsBuilder: IMetadataBuilder); override;
+
+    function newItem(AStatus: IStatus; AContext: IExternalContext;
+      AMetadata: IRoutineMetadata): IExternalTrigger; override;
   end;
 
 implementation
@@ -81,8 +117,8 @@ begin
 end;
 
 procedure TProcedureSimpleFactory<T>.setup(AStatus: IStatus;
-  AContext: IExternalContext; AMetadata: IRoutineMetadata; AInBuilder,
-  AOutBuilder: IMetadataBuilder);
+  AContext: IExternalContext; AMetadata: IRoutineMetadata;
+  AInBuilder, AOutBuilder: IMetadataBuilder);
 begin
 
 end;
@@ -101,8 +137,8 @@ begin
 end;
 
 procedure TFunctionSimpleFactory<T>.setup(AStatus: IStatus;
-  AContext: IExternalContext; AMetadata: IRoutineMetadata; AInBuilder,
-  AOutBuilder: IMetadataBuilder);
+  AContext: IExternalContext; AMetadata: IRoutineMetadata;
+  AInBuilder, AOutBuilder: IMetadataBuilder);
 begin
 
 end;
@@ -122,8 +158,8 @@ begin
 end;
 
 procedure TFunctionFactory<T>.setup(AStatus: IStatus;
-  AContext: IExternalContext; AMetadata: IRoutineMetadata; AInBuilder,
-  AOutBuilder: IMetadataBuilder);
+  AContext: IExternalContext; AMetadata: IRoutineMetadata;
+  AInBuilder, AOutBuilder: IMetadataBuilder);
 begin
 
 end;
@@ -143,11 +179,50 @@ begin
 end;
 
 procedure TProcedureFactory<T>.setup(AStatus: IStatus;
-  AContext: IExternalContext; AMetadata: IRoutineMetadata; AInBuilder,
-  AOutBuilder: IMetadataBuilder);
+  AContext: IExternalContext; AMetadata: IRoutineMetadata;
+  AInBuilder, AOutBuilder: IMetadataBuilder);
 begin
 
 end;
 
+{ TTriggerSimpleFactory<T> }
+
+procedure TTriggerSimpleFactory<T>.dispose;
+begin
+
+end;
+
+function TTriggerSimpleFactory<T>.newItem(AStatus: IStatus;
+  AContext: IExternalContext; AMetadata: IRoutineMetadata): IExternalTrigger;
+begin
+  Result := T.Create;
+end;
+
+procedure TTriggerSimpleFactory<T>.setup(AStatus: IStatus;
+  AContext: IExternalContext; AMetadata: IRoutineMetadata;
+  AFieldsBuilder: IMetadataBuilder);
+begin
+
+end;
+
+{ TTriggerFactory<T> }
+
+procedure TTriggerFactory<T>.dispose;
+begin
+
+end;
+
+function TTriggerFactory<T>.newItem(AStatus: IStatus;
+  AContext: IExternalContext; AMetadata: IRoutineMetadata): IExternalTrigger;
+begin
+  Result := T.Create;
+  (Result as T).Metadata := AMetadata;
+end;
+
+procedure TTriggerFactory<T>.setup(AStatus: IStatus; AContext: IExternalContext;
+  AMetadata: IRoutineMetadata; AFieldsBuilder: IMetadataBuilder);
+begin
+
+end;
 
 end.

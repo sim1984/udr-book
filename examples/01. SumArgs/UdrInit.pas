ea35102b85450e22b1afa3c0b1dc90c2318f3c6b
Simonov Denis <sim-mail@list.ru>
2020-06-17 20:42:22 +0300

    copyright

--- examples/01. SumArgs/UdrInit.pas
+++ examples/01. SumArgs/UdrInit.pas
@@ -1,4 +1,31 @@
-﻿unit UdrInit;
+﻿{
+ *	PROGRAM:	UDR samples.
+ *	MODULE:		UdrInit.pas
+ *	DESCRIPTION:	A sample simple UDR.
+ *
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by Adriano dos Santos
+ *  for the Firebird Open Source RDBMS project.
+ *
+ *  Copyright (c) 2008 Adriano dos Santos Fernandes <adrianosf@gmail.com>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________. 
+ *
+ *  20.05.2018 Simonov Denis - comments and simplificate trigger }
+
+unit UdrInit;
 
 {$IFDEF FPC}
 {$MODE DELPHI}{$H+}
--- examples/04. Split/UdrInit.pas
+++ examples/04. Split/UdrInit.pas
@@ -1,3 +1,28 @@
+{
+ *	PROGRAM:	UDR samples.
+ *	MODULE:		UdrInit.pas
+ *	DESCRIPTION:	A sample work with blob in extenal procedure.
+ *
+ *  The contents of this file are subject to the Initial
+ *  Developer's Public License Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ *
+ *  Software distributed under the License is distributed AS IS,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing rights
+ *  and limitations under the License.
+ *
+ *  The Original Code was created by Simonov Denis
+ *  for the book Writing UDR Firebird in Pascal.
+ *
+ *  Copyright (c) 2018 Simonov Denis <sim-mail@list.ru>
+ *  and all contributors signed below.
+ *
+ *  All Rights Reserved.
+ *  Contributor(s): ______________________________________. }
+
 unit UdrInit;
 
 {$IFDEF FPC}
